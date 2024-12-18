3a0d9789717c37669abe57772b3ac08ae2325198
Roylyn <roylynjoyestrera@gmail.com>
2024-12-19 01:54:23 +0800

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,105 +1,31 @@
 import { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router'
+import ListTodos from './components/ListTodos'
+import { SignIn } from './components/SignIn'
+import { SignUp } from './components/SignUp'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './firebase'
 import './App.css'
-import { db } from './firebase'
-import { collection, doc, getDocs, deleteDoc, addDoc, updateDoc, getDoc } from "firebase/firestore";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
-
-  const fetchTasks = async () => {
-    const collectionRef = collection(db, 'tasks');
-    const querySnapshot = await getDocs(collectionRef);
-
-    const tasks = querySnapshot.docs.map((task) => ({
-      id: task.id,
-      ...task.data()
-    }))
-    setTasks(tasks)
-  }
+  const [user, setUser] = useState('roylyn')
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
-
-  const deleteTask = async (id) => {
-
-    const docRef = doc(db, 'tasks', id)
-    await deleteDoc(docRef)
-
-    setTasks((prevTasks) => prevTasks.filter(task => task.id !== id))
-  }
-
-  const addTask = async (e) => {
-    e.preventDefault();
-    const collectionRef = collection(db, 'tasks');
-    await addDoc(collectionRef, {
-      title: title,
-      body: body,
-      status: 'pending'
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user)
     })
-    setTitle('')
-    setBody('')
-    alert('Task added')
-  }
 
-  const handleStatus = async (id) => {
-    try {
-      const itemRef = doc(db, 'tasks', id);
-      const currentTask = await getDoc(itemRef);
-      const currentStatus = currentTask.data().status;
-      const newStatus = currentStatus === 'pending' ? 'completed' : 'pending';
-
-      await updateDoc(itemRef, {
-        status: newStatus,
-      });
-
-      setTasks((prevTasks) =>
-        prevTasks.map((task) =>
-          task.id === id ? { ...task, status: newStatus } : task
-        )
-      );
-    } catch (error) {
-      console.log(error)
-    }
-
-  }
+    return unsubscribe
+  }, [])
 
   return (
-    <>
-      <div className="task-form">
-        <h3 id='TM'>Task Manager</h3>
-        <form onSubmit={addTask}>
-          <input type="text" name="title" id="title" placeholder="title" value={title} required onChange={(e) => setTitle(e.target.value)} />
-          <textarea name="desc" id="desc" placeholder="description" value={body} required onChange={(e) => setBody(e.target.value)}></textarea>
-          <button type="submit" id="button" onClick={() => { setTimeout(() => { window.location.reload() }, 1500) }}>Add task</button>
-        </form>
-      </div>
-
-      {
-        tasks.map((task) => (
-          <div key={task.id}>
-            <div>
-              Task title: {task.title}
-            </div>
-            <div>
-              Task body: {task.body}
-            </div>
-            <div>
-              Task status
-              <button onClick={() => {handleStatus(task.id)}}>
-                {task.status}
-              </button>
-            </div>
-            <button onClick={() => deleteTask(task.id)}>
-              Delete task
-            </button>
-          </div>
-
-        ))
-      }
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={user ? <ListTodos user={user} /> : <SignIn />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
   )
 }
 
